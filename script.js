2c2c436dada4a43042dc671f6ac5668ae9a95e46
Glitch (quackathon) <none>
2018-03-25 20:34:15 +0000

    💏🕶 Checkpoint ./index.html:789186/395 ./script.js:789172/87:789176/1509:789186/388 ./styles.css:789186/29 ./page-one.html:789186/105

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ console.log('hi');
 var problemsArray = [];
 var selectedProblem ;
 
+
+
 $(".add-problem-button").click(function(){
 
   var problem = $(".add-problem-input").val();
@@ -10,15 +12,23 @@ $(".add-problem-button").click(function(){
   
 });
 
+$(".problem").click(function() {
+    window.open("/page-one.html");
+});
+
 function addProblem(problem){
   problemsArray.push(problem);
   console.log(problemsArray);
   //alert(problemsArray);
-  updateBlackhole(problem);
+  
+  updateBlackhole("blackholeProblem",problem);
 }
+addProblem("problem");
+addProblem("Another problem");
 
-function updateBlackhole(blackhole){
-
+function updateBlackhole(blackhole,text){
+  
+  
   var element= document.createElement("div");
   
   $(element).click(function(){
@@ -26,18 +36,21 @@ function updateBlackhole(blackhole){
   window.open("/page-one.html");
     
   });
-    
+  
   
   element.classList.add("problem","reverse-rotating");
   
-  var node = document.createTextNode(problem);
+  var node = document.createTextNode(text);
 
   element.appendChild(node);
   
   var blackhole = document.getElementsByClassName(blackhole)[0];
   
   blackhole.appendChild(element);
-   
+  
+   var xy = getRandomPosition(element);
+	element.style.top = xy[0] + 'px';
+	element.style.left = xy[1] + 'px';
   
 }
 
@@ -50,12 +63,12 @@ $("problem").click(function(){
 
 addProblem("Gun Violence");
 
-
-
-function getRandomPosition(problem) {
-	var x = document.body.offsetHeight-problem.clientHeight;
-	var y = document.body.offsetWidth-problem.clientWidth;
+function getRandomPosition(element) {
+	var x = document.body.offsetHeight-element.clientHeight;
+	var y = document.body.offsetWidth-element.clientWidth;
 	var randomX = Math.floor(Math.random()*x);
 	var randomY = Math.floor(Math.random()*y);
 	return [randomX,randomY];
+  
 }
+
